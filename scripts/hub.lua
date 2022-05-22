63e882c3cc6f4a0cea6770b8375fee3c5a51a793
wrighttoby <89476497+wrighttoby@users.noreply.github.com>
2022-05-22 19:01:40 +0100

    hub.lua

---
+++ scripts/hub.lua
@@ -0,0 +1,549 @@
+local Scripts = Instance.new("ScrollingFrame")
+local UI1 = Instance.new("ImageButton")
+local UI2 = Instance.new("ImageButton")
+local UI3 = Instance.new("ImageButton")
+local UI4 = Instance.new("ImageButton")
+local UI5 = Instance.new("ImageButton")
+local UI6 = Instance.new("ImageButton")
+local UI7 = Instance.new("ImageButton")
+local UI8 = Instance.new("ImageButton")
+local UI9 = Instance.new("ImageButton")
+local UI10 = Instance.new("ImageButton")
+local UI11 = Instance.new("ImageButton")
+local UI12 = Instance.new("ImageButton")
+local UI13 = Instance.new("ImageButton")
+local UI14 = Instance.new("ImageButton")
+local UI15 = Instance.new("ImageButton")
+local UI16 = Instance.new("ImageButton")
+local UI17 = Instance.new("ImageButton")
+local Disc = Instance.new("ImageButton")
+local TXT2 = Instance.new("TextLabel")
+local TXT3 = Instance.new("TextLabel")
+local TXT4 = Instance.new("TextLabel")
+local TXT5 = Instance.new("TextLabel")
+local TXT6 = Instance.new("TextLabel")
+local TXT7 = Instance.new("TextLabel")
+local TXT8 = Instance.new("TextLabel")
+local TXT9 = Instance.new("TextLabel")
+local TXT10 = Instance.new("TextLabel")
+local TXT11 = Instance.new("TextLabel")
+local TXT12 = Instance.new("TextLabel")
+local TXT13 = Instance.new("TextLabel")
+local TXT14 = Instance.new("TextLabel")
+local TXT15 = Instance.new("TextLabel")
+local TXT16 = Instance.new("TextLabel")
+local TXT17 = Instance.new("TextLabel")
+local TXT18 = Instance.new("TextLabel")
+local Discord = Instance.new("TextLabel")
+local VER = Instance.new("TextLabel")
+
+--Properties:
+
+Scripts.Name = "Scripts"
+Scripts.Parent = game.StarterGui.ElectroHax.MainFrame
+Scripts.Active = true
+Scripts.BackgroundColor3 = Color3.fromRGB(77, 77, 77)
+Scripts.BorderSizePixel = 0
+Scripts.Position = UDim2.new(0.00189827697, 0, 0.0810439885, 0)
+Scripts.Size = UDim2.new(0.615934134, 0, 0.870879054, 0)
+Scripts.BottomImage = ""
+Scripts.ScrollBarThickness = 5
+Scripts.TopImage = ""
+
+UI1.Name = "UI1"
+UI1.Parent = Scripts
+UI1.BackgroundColor3 = Color3.fromRGB(77, 77, 77)
+UI1.BorderSizePixel = 0
+UI1.Position = UDim2.new(-0.00338373519, 0, -0.000565927476, 0)
+UI1.Size = UDim2.new(1.00338376, 0, 0.0385991558, 0)
+UI1.Image = "http://www.roblox.com/asset/?id=9683255382"
+
+UI2.Name = "UI2"
+UI2.Parent = Scripts
+UI2.BackgroundColor3 = Color3.fromRGB(77, 77, 77)
+UI2.BorderSizePixel = 0
+UI2.Position = UDim2.new(0, 0, 0.0516318716, 0)
+UI2.Size = UDim2.new(1.00338376, 0, 0.0385991558, 0)
+UI2.Image = "http://www.roblox.com/asset/?id=9683255382"
+
+UI3.Name = "UI3"
+UI3.Parent = Scripts
+UI3.BackgroundColor3 = Color3.fromRGB(77, 77, 77)
+UI3.BorderSizePixel = 0
+UI3.Position = UDim2.new(-0.00338373519, 0, 0.104516484, 0)
+UI3.Size = UDim2.new(1.00338376, 0, 0.0385991558, 0)
+UI3.Image = "http://www.roblox.com/asset/?id=9683255382"
+
+UI4.Name = "UI4"
+UI4.Parent = Scripts
+UI4.BackgroundColor3 = Color3.fromRGB(77, 77, 77)
+UI4.BorderSizePixel = 0
+UI4.Position = UDim2.new(-0.00338373519, 0, 0.156027466, 0)
+UI4.Size = UDim2.new(1.00338376, 0, 0.0385991558, 0)
+UI4.Image = "http://www.roblox.com/asset/?id=9683255382"
+
+UI5.Name = "UI5"
+UI5.Parent = Scripts
+UI5.BackgroundColor3 = Color3.fromRGB(77, 77, 77)
+UI5.BorderSizePixel = 0
+UI5.Position = UDim2.new(-0.00338373519, 0, 0.207538456, 0)
+UI5.Size = UDim2.new(1.00338376, 0, 0.0385991558, 0)
+UI5.Image = "http://www.roblox.com/asset/?id=9683255382"
+
+UI6.Name = "UI6"
+UI6.Parent = Scripts
+UI6.BackgroundColor3 = Color3.fromRGB(77, 77, 77)
+UI6.BorderSizePixel = 0
+UI6.Position = UDim2.new(-0.00338373519, 0, 0.25973627, 0)
+UI6.Size = UDim2.new(1.00338376, 0, 0.0385991558, 0)
+UI6.Image = "http://www.roblox.com/asset/?id=9683255382"
+
+UI7.Name = "UI7"
+UI7.Parent = Scripts
+UI7.BackgroundColor3 = Color3.fromRGB(77, 77, 77)
+UI7.BorderSizePixel = 0
+UI7.Position = UDim2.new(-0.00338373519, 0, 0.312620878, 0)
+UI7.Size = UDim2.new(1.00338376, 0, 0.0385991558, 0)
+UI7.Image = "http://www.roblox.com/asset/?id=9683255382"
+
+UI8.Name = "UI8"
+UI8.Parent = Scripts
+UI8.BackgroundColor3 = Color3.fromRGB(77, 77, 77)
+UI8.BorderSizePixel = 0
+UI8.Position = UDim2.new(-0.00338373519, 0, 0.366192311, 0)
+UI8.Size = UDim2.new(1.00338376, 0, 0.0385991558, 0)
+UI8.Image = "http://www.roblox.com/asset/?id=9683255382"
+
+UI9.Name = "UI9"
+UI9.Parent = Scripts
+UI9.BackgroundColor3 = Color3.fromRGB(77, 77, 77)
+UI9.BorderSizePixel = 0
+UI9.Position = UDim2.new(-0.00338373519, 0, 0.421137363, 0)
+UI9.Size = UDim2.new(1.00338376, 0, 0.0385991558, 0)
+UI9.Image = "http://www.roblox.com/asset/?id=9683255382"
+
+UI10.Name = "UI10"
+UI10.Parent = Scripts
+UI10.BackgroundColor3 = Color3.fromRGB(77, 77, 77)
+UI10.BorderSizePixel = 0
+UI10.Position = UDim2.new(-0.00338373519, 0, 0.476082414, 0)
+UI10.Size = UDim2.new(1.00338376, 0, 0.0385991558, 0)
+UI10.Image = "http://www.roblox.com/asset/?id=9683255382"
+
+UI11.Name = "UI11"
+UI11.Parent = Scripts
+UI11.BackgroundColor3 = Color3.fromRGB(77, 77, 77)
+UI11.BorderSizePixel = 0
+UI11.Position = UDim2.new(0, 0, 0.530340672, 0)
+UI11.Size = UDim2.new(1.00338376, 0, 0.0385991558, 0)
+UI11.Image = "http://www.roblox.com/asset/?id=9683255382"
+
+UI12.Name = "UI12"
+UI12.Parent = Scripts
+UI12.BackgroundColor3 = Color3.fromRGB(77, 77, 77)
+UI12.BorderSizePixel = 0
+UI12.Position = UDim2.new(-0.00338373519, 0, 0.584598899, 0)
+UI12.Size = UDim2.new(1.00338376, 0, 0.0385991558, 0)
+UI12.Image = "http://www.roblox.com/asset/?id=9683255382"
+
+UI13.Name = "UI13"
+UI13.Parent = Scripts
+UI13.BackgroundColor3 = Color3.fromRGB(77, 77, 77)
+UI13.BorderSizePixel = 0
+UI13.Position = UDim2.new(0, 0, 0.639543891, 0)
+UI13.Size = UDim2.new(1.00338376, 0, 0.0385991558, 0)
+UI13.Image = "http://www.roblox.com/asset/?id=9683255382"
+
+UI14.Name = "UI14"
+UI14.Parent = Scripts
+UI14.BackgroundColor3 = Color3.fromRGB(77, 77, 77)
+UI14.BorderSizePixel = 0
+UI14.Position = UDim2.new(-0.00338373519, 0, 0.693802118, 0)
+UI14.Size = UDim2.new(1.00338376, 0, 0.0385991558, 0)
+UI14.Image = "http://www.roblox.com/asset/?id=9683255382"
+
+UI15.Name = "UI15"
+UI15.Parent = Scripts
+UI15.BackgroundColor3 = Color3.fromRGB(77, 77, 77)
+UI15.BorderSizePixel = 0
+UI15.Position = UDim2.new(0, 0, 0.748060346, 0)
+UI15.Size = UDim2.new(1.00338376, 0, 0.0385991558, 0)
+UI15.Image = "http://www.roblox.com/asset/?id=9683255382"
+
+UI16.Name = "UI16"
+UI16.Parent = Scripts
+UI16.BackgroundColor3 = Color3.fromRGB(77, 77, 77)
+UI16.BorderSizePixel = 0
+UI16.Position = UDim2.new(-0.00676747039, 0, 0.803692102, 0)
+UI16.Size = UDim2.new(1.00338376, 0, 0.0385991558, 0)
+UI16.Image = "http://www.roblox.com/asset/?id=9683255382"
+
+UI17.Name = "UI17"
+UI17.Parent = Scripts
+UI17.BackgroundColor3 = Color3.fromRGB(77, 77, 77)
+UI17.BorderSizePixel = 0
+UI17.Position = UDim2.new(-0.00676747039, 0, 0.860010862, 0)
+UI17.Size = UDim2.new(1.00338376, 0, 0.0385991558, 0)
+UI17.Image = "http://www.roblox.com/asset/?id=9683255382"
+
+Disc.Name = "Disc"
+Disc.Parent = Scripts
+Disc.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Disc.BorderSizePixel = 0
+Disc.Position = UDim2.new(-0.00676747039, 0, 0.935560286, 0)
+Disc.Size = UDim2.new(1.00338376, 0, 0.0385991558, 0)
+Disc.Image = "http://www.roblox.com/asset/?id=9683255382"
+
+TXT2.Name = "TXT2"
+TXT2.Parent = Scripts
+TXT2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TXT2.BackgroundTransparency = 1.000
+TXT2.Position = UDim2.new(0.00676747039, 0, 0.00380732911, 0)
+TXT2.Size = UDim2.new(0.976313949, 0, 0.0291820727, 0)
+TXT2.Font = Enum.Font.SourceSans
+TXT2.Text = "EXAMPLE"
+TXT2.TextColor3 = Color3.fromRGB(255, 204, 0)
+TXT2.TextScaled = true
+TXT2.TextSize = 14.000
+TXT2.TextWrapped = true
+
+TXT3.Name = "TXT3"
+TXT3.Parent = Scripts
+TXT3.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TXT3.BackgroundTransparency = 1.000
+TXT3.Position = UDim2.new(0.00676747039, 0, 0.0560051277, 0)
+TXT3.Size = UDim2.new(0.976313949, 0, 0.0291820727, 0)
+TXT3.Font = Enum.Font.SourceSans
+TXT3.Text = "EXAMPLE"
+TXT3.TextColor3 = Color3.fromRGB(255, 204, 0)
+TXT3.TextScaled = true
+TXT3.TextSize = 14.000
+TXT3.TextWrapped = true
+
+TXT4.Name = "TXT4"
+TXT4.Parent = Scripts
+TXT4.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TXT4.BackgroundTransparency = 1.000
+TXT4.Position = UDim2.new(0.00676747039, 0, 0.108889744, 0)
+TXT4.Size = UDim2.new(0.976313949, 0, 0.0291820727, 0)
+TXT4.Font = Enum.Font.SourceSans
+TXT4.Text = "EXAMPLE"
+TXT4.TextColor3 = Color3.fromRGB(255, 204, 0)
+TXT4.TextScaled = true
+TXT4.TextSize = 14.000
+TXT4.TextWrapped = true
+
+TXT5.Name = "TXT5"
+TXT5.Parent = Scripts
+TXT5.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TXT5.BackgroundTransparency = 1.000
+TXT5.Position = UDim2.new(0.00676747039, 0, 0.160400733, 0)
+TXT5.Size = UDim2.new(0.976313949, 0, 0.0291820727, 0)
+TXT5.Font = Enum.Font.SourceSans
+TXT5.Text = "EXAMPLE"
+TXT5.TextColor3 = Color3.fromRGB(255, 204, 0)
+TXT5.TextScaled = true
+TXT5.TextSize = 14.000
+TXT5.TextWrapped = true
+
+TXT6.Name = "TXT6"
+TXT6.Parent = Scripts
+TXT6.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TXT6.BackgroundTransparency = 1.000
+TXT6.Position = UDim2.new(0.0101512056, 0, 0.211911723, 0)
+TXT6.Size = UDim2.new(0.976313949, 0, 0.0291820727, 0)
+TXT6.Font = Enum.Font.SourceSans
+TXT6.Text = "EXAMPLE"
+TXT6.TextColor3 = Color3.fromRGB(255, 204, 0)
+TXT6.TextScaled = true
+TXT6.TextSize = 14.000
+TXT6.TextWrapped = true
+
+TXT7.Name = "TXT7"
+TXT7.Parent = Scripts
+TXT7.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TXT7.BackgroundTransparency = 1.000
+TXT7.Position = UDim2.new(0.0203024112, 0, 0.264109522, 0)
+TXT7.Size = UDim2.new(0.976313949, 0, 0.0291820727, 0)
+TXT7.Font = Enum.Font.SourceSans
+TXT7.Text = "EXAMPLE"
+TXT7.TextColor3 = Color3.fromRGB(255, 204, 0)
+TXT7.TextScaled = true
+TXT7.TextSize = 14.000
+TXT7.TextWrapped = true
+
+TXT8.Name = "TXT8"
+TXT8.Parent = Scripts
+TXT8.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TXT8.BackgroundTransparency = 1.000
+TXT8.Position = UDim2.new(0.0101512056, 0, 0.316994131, 0)
+TXT8.Size = UDim2.new(0.976313949, 0, 0.0291820727, 0)
+TXT8.Font = Enum.Font.SourceSans
+TXT8.Text = "EXAMPLE"
+TXT8.TextColor3 = Color3.fromRGB(255, 204, 0)
+TXT8.TextScaled = true
+TXT8.TextSize = 14.000
+TXT8.TextWrapped = true
+
+TXT9.Name = "TXT9"
+TXT9.Parent = Scripts
+TXT9.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TXT9.BackgroundTransparency = 1.000
+TXT9.Position = UDim2.new(0.0101512056, 0, 0.370565563, 0)
+TXT9.Size = UDim2.new(0.976313949, 0, 0.0291820727, 0)
+TXT9.Font = Enum.Font.SourceSans
+TXT9.Text = "EXAMPLE"
+TXT9.TextColor3 = Color3.fromRGB(255, 204, 0)
+TXT9.TextScaled = true
+TXT9.TextSize = 14.000
+TXT9.TextWrapped = true
+
+TXT10.Name = "TXT10"
+TXT10.Parent = Scripts
+TXT10.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TXT10.BackgroundTransparency = 1.000
+TXT10.Position = UDim2.new(0.0101512056, 0, 0.425510615, 0)
+TXT10.Size = UDim2.new(0.976313949, 0, 0.0291820727, 0)
+TXT10.Font = Enum.Font.SourceSans
+TXT10.Text = "EXAMPLE"
+TXT10.TextColor3 = Color3.fromRGB(255, 204, 0)
+TXT10.TextScaled = true
+TXT10.TextSize = 14.000
+TXT10.TextWrapped = true
+
+TXT11.Name = "TXT11"
+TXT11.Parent = Scripts
+TXT11.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TXT11.BackgroundTransparency = 1.000
+TXT11.Position = UDim2.new(0.0101512056, 0, 0.480455667, 0)
+TXT11.Size = UDim2.new(0.976313949, 0, 0.0291820727, 0)
+TXT11.Font = Enum.Font.SourceSans
+TXT11.Text = "EXAMPLE"
+TXT11.TextColor3 = Color3.fromRGB(255, 204, 0)
+TXT11.TextScaled = true
+TXT11.TextSize = 14.000
+TXT11.TextWrapped = true
+
+TXT12.Name = "TXT12"
+TXT12.Parent = Scripts
+TXT12.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TXT12.BackgroundTransparency = 1.000
+TXT12.Position = UDim2.new(0.00676747039, 0, 0.535400748, 0)
+TXT12.Size = UDim2.new(0.976313949, 0, 0.0291820727, 0)
+TXT12.Font = Enum.Font.SourceSans
+TXT12.Text = "EXAMPLE"
+TXT12.TextColor3 = Color3.fromRGB(255, 204, 0)
+TXT12.TextScaled = true
+TXT12.TextSize = 14.000
+TXT12.TextWrapped = true
+
+TXT13.Name = "TXT13"
+TXT13.Parent = Scripts
+TXT13.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TXT13.BackgroundTransparency = 1.000
+TXT13.Position = UDim2.new(0.0101512056, 0, 0.588972151, 0)
+TXT13.Size = UDim2.new(0.976313949, 0, 0.0291820727, 0)
+TXT13.Font = Enum.Font.SourceSans
+TXT13.Text = "EXAMPLE"
+TXT13.TextColor3 = Color3.fromRGB(255, 204, 0)
+TXT13.TextScaled = true
+TXT13.TextSize = 14.000
+TXT13.TextWrapped = true
+
+TXT14.Name = "TXT14"
+TXT14.Parent = Scripts
+TXT14.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TXT14.BackgroundTransparency = 1.000
+TXT14.Position = UDim2.new(0.0101512056, 0, 0.643917203, 0)
+TXT14.Size = UDim2.new(0.976313949, 0, 0.0291820727, 0)
+TXT14.Font = Enum.Font.SourceSans
+TXT14.Text = "EXAMPLE"
+TXT14.TextColor3 = Color3.fromRGB(255, 204, 0)
+TXT14.TextScaled = true
+TXT14.TextSize = 14.000
+TXT14.TextWrapped = true
+
+TXT15.Name = "TXT15"
+TXT15.Parent = Scripts
+TXT15.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TXT15.BackgroundTransparency = 1.000
+TXT15.Position = UDim2.new(0.0101512056, 0, 0.69817543, 0)
+TXT15.Size = UDim2.new(0.976313949, 0, 0.0291820727, 0)
+TXT15.Font = Enum.Font.SourceSans
+TXT15.Text = "EXAMPLE"
+TXT15.TextColor3 = Color3.fromRGB(255, 204, 0)
+TXT15.TextScaled = true
+TXT15.TextSize = 14.000
+TXT15.TextWrapped = true
+
+TXT16.Name = "TXT16"
+TXT16.Parent = Scripts
+TXT16.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TXT16.BackgroundTransparency = 1.000
+TXT16.Position = UDim2.new(0.0101512056, 0, 0.752433658, 0)
+TXT16.Size = UDim2.new(0.976313949, 0, 0.0291820727, 0)
+TXT16.Font = Enum.Font.SourceSans
+TXT16.Text = "EXAMPLE"
+TXT16.TextColor3 = Color3.fromRGB(255, 204, 0)
+TXT16.TextScaled = true
+TXT16.TextSize = 14.000
+TXT16.TextWrapped = true
+
+TXT17.Name = "TXT17"
+TXT17.Parent = Scripts
+TXT17.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TXT17.BackgroundTransparency = 1.000
+TXT17.Position = UDim2.new(0.0101512056, 0, 0.808065534, 0)
+TXT17.Size = UDim2.new(0.976313949, 0, 0.0291820727, 0)
+TXT17.Font = Enum.Font.SourceSans
+TXT17.Text = "EXAMPLE"
+TXT17.TextColor3 = Color3.fromRGB(255, 204, 0)
+TXT17.TextScaled = true
+TXT17.TextSize = 14.000
+TXT17.TextWrapped = true
+
+TXT18.Name = "TXT18"
+TXT18.Parent = Scripts
+TXT18.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TXT18.BackgroundTransparency = 1.000
+TXT18.Position = UDim2.new(0.0101512056, 0, 0.864384234, 0)
+TXT18.Size = UDim2.new(0.976313949, 0, 0.0291820727, 0)
+TXT18.Font = Enum.Font.SourceSans
+TXT18.Text = "EXAMPLE"
+TXT18.TextColor3 = Color3.fromRGB(255, 204, 0)
+TXT18.TextScaled = true
+TXT18.TextSize = 14.000
+TXT18.TextWrapped = true
+
+Discord.Name = "Discord"
+Discord.Parent = Scripts
+Discord.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Discord.BackgroundTransparency = 1.000
+Discord.Position = UDim2.new(0.00676747039, 0, 0.939933717, 0)
+Discord.Size = UDim2.new(0.976313949, 0, 0.0291820727, 0)
+Discord.Font = Enum.Font.SourceSans
+Discord.Text = "https://discord.io/ElectroHub"
+Discord.TextColor3 = Color3.fromRGB(255, 204, 0)
+Discord.TextScaled = true
+Discord.TextSize = 14.000
+Discord.TextWrapped = true
+
+VER.Name = "VER"
+VER.Parent = Scripts
+VER.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+VER.BackgroundTransparency = 1.000
+VER.Position = UDim2.new(0.00676747039, 0, 0.980930924, 0)
+VER.Size = UDim2.new(0.976313949, 0, 0.013389715, 0)
+VER.Font = Enum.Font.SourceSans
+VER.Text = "Version 1.0 BETA"
+VER.TextColor3 = Color3.fromRGB(255, 204, 0)
+VER.TextScaled = true
+VER.TextSize = 14.000
+VER.TextWrapped = true
+
+-- Scripts:
+
+local function YJHPKTY_fake_script() -- Scripts.Scripts 
+	local script = Instance.new('LocalScript', Scripts)
+
+	script.Parent = game.CoreGui.ElectroHax.MainFrame
+	
+	--[[
+	#####################################################################
+	#####################################################################
+	#####################################################################
+	--]]
+	
+	
+	
+	--Scripts Here
+	--If You Know How To Code Then You Can Remix Your Own Version And Make Your Own Version
+	--NOTE: YOU CANNOT MAKE IT PUBLIC WITHOUT UPLOADING A COPY TO THE DISCORD SERVER
+	--NOTE: DO NOT CHANGE THE DISCORD LINK AT ANY COST
+	
+	--What Should The Text Of The Buttons say?
+	
+	script.Parent.TXT2.Text = ""
+	script.Parent.TXT3.Text = ""
+	script.Parent.TXT4.Text = ""
+	script.Parent.TXT5.Text = ""
+	script.Parent.TXT6.Text = ""
+	script.Parent.TXT7.Text = ""
+	script.Parent.TXT8.Text = ""
+	script.Parent.TXT9.Text = ""
+	script.Parent.TXT10.Text = ""
+	script.Parent.TXT11.Text = ""
+	script.Parent.TXT12.Text = ""
+	script.Parent.TXT13.Text = ""
+	script.Parent.TXT14.Text = ""
+	script.Parent.TXT15.Text = ""
+	script.Parent.TXT16.Text = ""
+	script.Parent.TXT17.Text = ""
+	script.Parent.TXT18.Text = ""
+	
+	--What Does The Button do?
+	script.Parent.UI1.MouseButton1Click:Connect(function()
+		print("")
+	end)
+	script.Parent.UI2.MouseButton1Click:Connect(function()
+		
+	end)
+	script.Parent.UI3.MouseButton1Click:Connect(function()
+		
+	end)
+	script.Parent.UI4.MouseButton1Click:Connect(function()
+		
+	end)
+	script.Parent.UI5.MouseButton1Click:Connect(function()
+		
+	end)
+	script.Parent.UI6.MouseButton1Click:Connect(function()
+		
+	end)
+	script.Parent.UI7.MouseButton1Click:Connect(function()
+		
+	end)
+	script.Parent.UI8.MouseButton1Click:Connect(function()
+		
+	end)
+	script.Parent.UI9.MouseButton1Click:Connect(function()
+		
+	end)
+	script.Parent.UI10.MouseButton1Click:Connect(function()
+		
+	end)
+	script.Parent.UI11.MouseButton1Click:Connect(function()
+		
+	end)
+	script.Parent.UI12.MouseButton1Click:Connect(function()
+		
+	end)
+	script.Parent.UI13.MouseButton1Click:Connect(function()
+		
+	end)
+	script.Parent.UI14.MouseButton1Click:Connect(function()
+		
+	end)
+	script.Parent.UI15.MouseButton1Click:Connect(function()
+		
+	end)
+	script.Parent.UI16.MouseButton1Click:Connect(function()
+		
+	end)
+	script.Parent.UI17.MouseButton1Click:Connect(function()
+		
+	end)
+	
+	
+	
+	--[[
+	#####################################################################
+	#####################################################################
+	#####################################################################
+	--]]
+end
+coroutine.wrap(YJHPKTY_fake_script)()
+
+
+
+
